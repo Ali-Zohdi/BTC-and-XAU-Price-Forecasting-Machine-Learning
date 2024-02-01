3496a018945416b1cf2147e77adaf0cd69c42970
Ali Zohdi <alii.zohdii@gmail.com>
2024-02-01 11:23:57 +0330

    PatchTST single output channel

--- Transformer_EncDec.py
+++ Transformer_EncDec.py
@@ -297,41 +297,25 @@ class Flatten_Head_For_Single_Output(nn.Module):
         self.individual = individual
         self.n_vars = n_vars
         
-        if self.individual:
-            self.linears = nn.ModuleList()
-            self.dropouts = nn.ModuleList()
-            self.flattens = nn.ModuleList()
-            for i in range(self.n_vars):
-                self.flatten = nn.Flatten(start_dim=-3)
-                self.linear = nn.Linear(nf * n_vars, target_window) 
-                self.dropout = nn.Dropout(head_dropout)
-        else:
-            self.flatten = nn.Flatten(start_dim=-3)
-            self.linear = nn.Linear(nf * n_vars, target_window) 
-            self.dropout = nn.Dropout(head_dropout)
+        # if self.individual:
+        #     self.linears = nn.ModuleList()
+        #     self.dropouts = nn.ModuleList()
+        #     self.flattens = nn.ModuleList()
+        #     for i in range(self.n_vars):
+        #         self.flattens.append(nn.Flatten(start_dim=-3))
+        #         self.linears.append(nn.Linear(nf, target_window))
+        #         self.dropouts.append(nn.Dropout(head_dropout))
+        # else:
+        self.flatten = nn.Flatten(start_dim=-3)
+        self.linear = nn.Linear(nf * n_vars, target_window) 
+        self.dropout = nn.Dropout(head_dropout)
             
-    # def forward(self, x):                                 # x: [bs x nvars x d_model x patch_num]
-    #     x = self.flatten(x)                               # x: [bs x nvars * d_model * patch_num]
-    #     x = self.linear(x)                                # x: [bs x target_window]
-    #     x = self.dropout(x)
-    #     return x
     def forward(self, x):                                 # x: [bs x nvars x d_model x patch_num]
-        if self.individual:
-            x_out = []
-            for i in range(self.n_vars):
-                z = self.flattens[i](x[:,i,:,:])          # z: [bs x d_model * patch_num]
-                z = self.linears[i](z)                    # z: [bs x target_window]
-                z = self.dropouts[i](z)
-                x_out.append(z)
-            x = torch.stack(x_out, dim=1)                 # x: [bs x nvars x target_window]
-        else:
-            x = self.flatten(x)
-            x = self.linear(x)
-            x = self.dropout(x)
-        return x        
+        x = self.flatten(x)                               # x: [bs x nvars * d_model * patch_num]
+        x = self.linear(x)                                # x: [bs x target_window]
+        x = self.dropout(x)
+        return x       
         
-    
-    
 class TSTiEncoder(nn.Module):  #i means channel-independent
     def __init__(self, c_in, patch_num, patch_len, max_seq_len=1024,
                  n_layers=3, d_model=128, n_heads=16, d_k=None, d_v=None,
